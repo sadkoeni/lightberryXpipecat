605d6944610afa696a37696f0516a941167bba4f
sadkoeni <stephan.koenigstorfer@gmail.com>
2025-06-13 14:01:02 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 python setup.py
-cp lightberry.service /etc/systemd/system/
+sudo cp lightberry.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sydo systemctl restart lightberry
+sudo systemctl restart lightberry
